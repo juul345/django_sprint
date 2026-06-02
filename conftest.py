26067b6513f0acdec88f77edb3b814e9fa6a9604
juul345 <juul345@users.noreply.github.com>
2026-06-02 10:53:27 +0300

    Исправление импорта для тестов

--- conftest.py
+++ conftest.py
@@ -1,5 +1,13 @@
+import os
 import sys
 from pathlib import Path
 
 # Добавляем корневую папку в PYTHONPATH
 sys.path.insert(0, str(Path(__file__).parent))
+
+# Устанавливаем настройки Django
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'blogicum.settings')
+
+# Инициализируем Django
+import django
+django.setup()
